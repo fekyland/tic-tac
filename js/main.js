a26a5a4dc39e63911b9e25822264cee011662214
federico <federicosicoli@gmail.com>
2022-11-20 09:47:51 +0100

    cambios en resultados y diseño

--- js/main.js
+++ js/main.js
@@ -3,26 +3,30 @@
 
 let esJugadorX = true
 console.log( 'jugadorx', esJugadorX) 
+let jugador1  = "jugador 1"
+let jugador2  = "jugador 2"
 
 
 const playerClick = (ev)=> {
    // console.log(ev.target.innerHtml)
 
-let ticTac = ev.target
+let ticTac = ev.target                  //
 let boxValue = ev.target.innerHTML
-
+            console.log("turno  "  + jugador2 )
     if (boxValue == "") {
         if (esJugadorX){
-            ticTac.innerHTML = 'X'
+            document.getElementById("turno").innerHTML= jugador2
+            console.log("turno  "  + jugador2)
+            ticTac.innerHTML = '🍌'
             esJugadorX = false
         }
         else {
-            ticTac.innerHTML = 'O'
+            document.getElementById("turno").innerHTML= jugador1
+            ticTac.innerHTML = '🥝'
             esJugadorX = true
-         }
+            console.log("turno  "  + jugador2 )
+         }   
       }
-   
-     
         let boxValue1 = document.getElementById(`casiUno`).innerHTML;
         let boxValue2 = document.getElementById(`casiDos`).innerHTML;
         let boxValue3 = document.getElementById(`casiTres`).innerHTML;
@@ -33,41 +37,76 @@ let boxValue = ev.target.innerHTML
         let boxValue8 = document.getElementById(`casiOcho`).innerHTML;
         let boxValue9 = document.getElementById(`casiNueve`).innerHTML;
     
-        let xGanador = (boxValue1 == "X" && boxValue2 == "X" && boxValue3 == "X") ||
-                       (boxValue1 == "X" && boxValue4 == "X" && boxValue7 == "X") ||
-                       (boxValue2 == "X" && boxValue5 == "X" && boxValue8 == "X") ||
-                       (boxValue3 == "X" && boxValue6 == "X" && boxValue9 == "X") ||
-                       (boxValue1 == "X" && boxValue5 == "X" && boxValue9 == "X") ||
-                       (boxValue7 == "X" && boxValue8 == "X" && boxValue9 == "X") ||
-                       (boxValue3 == "X" && boxValue5 == "X" && boxValue7 == "X")
+        let xGanador = (boxValue1 == "🍌" && boxValue2 == "🍌" && boxValue3 == "🍌") ||
+                       (boxValue4 == "🍌" && boxValue5 == "🍌" && boxValue6 == "🍌") ||
+                       (boxValue1 == "🍌" && boxValue4 == "🍌" && boxValue7 == "🍌") ||
+                       (boxValue2 == "🍌" && boxValue5 == "🍌" && boxValue8 == "🍌") ||
+                       (boxValue3 == "🍌" && boxValue6 == "🍌" && boxValue9 == "🍌") ||
+                       (boxValue1 == "🍌" && boxValue5 == "🍌" && boxValue9 == "🍌") ||
+                       (boxValue7 == "🍌" && boxValue8 == "🍌" && boxValue9 == "🍌") ||
+                       (boxValue3 == "🍌" && boxValue5 == "🍌" && boxValue7 == "🍌")
 
-        let oGanador = (boxValue1 == "O" && boxValue2 == "O" && boxValue3 == "O") ||
-                       (boxValue1 == "O" && boxValue4 == "O" && boxValue7 == "O") ||
-                       (boxValue2 == "O" && boxValue5 == "O" && boxValue8 == "O") ||
-                       (boxValue3 == "O" && boxValue6 == "O" && boxValue9 == "O") ||
-                       (boxValue1 == "O" && boxValue5 == "O" && boxValue9 == "O") ||
-                       (boxValue7 == "O" && boxValue8 == "O" && boxValue9 == "O") ||
-                       (boxValue3 == "O" && boxValue5 == "O" && boxValue7 == "O")
-
+        let oGanador = (boxValue1 == "🥝" && boxValue2 == "🥝" && boxValue3 == "🥝") ||
+                       (boxValue4 == "🥝" && boxValue5 == "🥝" && boxValue6 == "🥝") || 
+                       (boxValue1 == "🥝" && boxValue4 == "🥝" && boxValue7 == "🥝") ||
+                       (boxValue2 == "🥝" && boxValue5 == "🥝" && boxValue8 == "🥝") ||
+                       (boxValue3 == "🥝" && boxValue6 == "🥝" && boxValue9 == "🥝") ||
+                       (boxValue1 == "🥝" && boxValue5 == "🥝" && boxValue9 == "🥝") ||
+                       (boxValue7 == "🥝" && boxValue8 == "🥝" && boxValue9 == "🥝") ||
+                       (boxValue3 == "🥝" && boxValue5 == "🥝" && boxValue7 == "🥝")
+        
     
         let tablero = [boxValue1, boxValue2,boxValue3,boxValue4,boxValue5,boxValue6,
                 boxValue7,boxValue8,boxValue9]
-        
-         console.log(tablero)
 
-        if (xGanador){
-            console.log("ganador es x")
-            
-        }
-        if (oGanador){
-            console.log("ganador es O")
-           
-        }
-        
+
+        console.log(tablero)
     
+        if (xGanador){
+            console.log("ganador es 🍌" + jugador1)
+            document.getElementById("textoResultado").innerHTML="el ganador es " 
+            document.getElementById("resultadoNombre").innerHTML= jugador1
+        }
+        else if (oGanador){
+            console.log("ganador es 🥝" + jugador2)
+            document.getElementById("textoResultado").innerHTML="el ganador es "
+            document.getElementById("resultadoNombre").innerHTML= jugador2
+        }else if(
+            !xGanador && !oGanador &&
+            boxValue1 !== "" &&
+            boxValue2 !== "" &&
+            boxValue3 !== "" &&
+            boxValue4 !== "" &&
+            boxValue5 !== "" &&
+            boxValue6 !== "" &&
+            boxValue7 !== "" &&
+            boxValue8 !== "" &&
+            boxValue9 !== ""
+            ){
+              document.getElementById("textoResultado").innerHTML="el resultado"
+              document.getElementById("resultadoNombre").innerHTML= "empate"
+            }
+           
+
+            
+       
+          /*  let ticTac = ev.target                  //
+            let boxValue = ev.target.innerHTML*/
+        
     }
 
-    
+    function iniciarJuego() {
+        // document.getElementsByClassName("ticTac").innerHTML = ""
+        location.reload()
+        console.log("wowww")
+    }
 
 
-        
+        function handleOnChange(event){
+               
+          if(event.target.name === "jugador1"){
+            jugador1  = event.target.value;
+          }else{
+            jugador2  = event.target.value;
+          }
+        }
