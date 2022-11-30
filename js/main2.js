b6c470c0b02f4dfe8454277ba2729fe2c5040dee
federico <federicosicoli@gmail.com>
2022-11-30 09:35:44 +0100

    agrego pagina inicial y popup resultado

---
+++ js/main2.js
@@ -0,0 +1,14 @@
+document.getElementById("textoResultado").innerHTML="el ganador es "
+document.getElementById("resultadoNombre").innerHTML= jugador1
+document.getElementById("resulNombre").innerHTML= jugador1
+
+
+
+
+document.getElementById("resultadoNombre").innerHTML= jugador2
+document.getElementById("resultadoNombre").innerHTML= 
+
+
+document.getElementById("resulNombre").innerHTML= jugador1
+          console.log("ganador es ❌" + jugador1)
+        
